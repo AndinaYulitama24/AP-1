64753a3d960482b208199e801ea63add303da5e9
Andina_YL <andinayulitama@gmail.com>
2025-03-17 09:33:36 +0700

    Latihan.cpp

--- pertemuan3 lab1/Latihan.cpp
+++ pertemuan3 lab1/Latihan.cpp
@@ -8,12 +8,12 @@ int main(){
     cout << "masukan nilai: " ;
     cin >> nilai;
 
-    if (nilai % 5==0){
-        cout<<"masuk bilang kelipan 5\n";
-    }
-    else if (nilai % 10==0)
-    {
+    if (nilai % 10==0){
         cout<<"masuk bilang kelipan 10\n";
+    }
+    else if (nilai % 5==0)
+    {
+        cout<<"masuk bilang kelipan 5\n";
     }
     else{
         cout<<"tidak masuk keduannya\n";

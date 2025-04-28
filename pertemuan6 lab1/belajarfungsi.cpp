95971d59b96db393db0136c938a865982567907a
Andina_YL <andinayulitama@gmail.com>
2025-04-28 09:45:51 +0700

    Pertemuan 6 : Fungsi dan Header

---
+++ pertemuan6 lab1/belajarfungsi.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+void sapa(string nama){
+    cout<<" halo "<<nama<<"! Selamat belajar c++ "<<endl;
+}
+int main(){
+    string namapengguna ="Andin";
+
+    sapa(namapengguna);
+    return 0;
+}

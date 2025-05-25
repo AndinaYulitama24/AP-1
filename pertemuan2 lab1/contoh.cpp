b85f48388bdd8c5a0a11286d7f71bb3c484c79c6
Andina_YL <andinayulitama@gmail.com>
2025-05-25 17:47:30 +0700

    Pertemuan 2 : Pengunaan Operator

---
+++ pertemuan2 lab1/contoh.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    //menghitung panjang
+    float luas, p, l; //untuk menyimpan bilangan desimal
+
+    cout << "masukan panjang :";
+    cin >> p;
+
+    cout << "masukan lebar :";
+    cin >> l;
+
+    luas = p * l;
+    cout <<"luas ="<< luas << endl;
+
+    return 0;
+}
---
+++ pertemuan2 lab1/operator.cpp
@@ -0,0 +1,106 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+
+    system("CLS");
+
+    //assigment Operator(=)
+    a= 3;
+    b= 5;
+
+    /*assigment operator(+,-,/,*,%),yaitu operator yang digunakan untuk menetapkan nilai ke suatu variabel atau properti*/
+
+    //int tambah = a + b;
+    //int kurang = a - b;
+    //int kali = a * b;
+    //float bagi = (float)a / (float)b;// type casting = mengubah tipe data dari sebuah variabel
+    //int modulo = a % b;
+
+   // cout << "hasil penjumlahan :"<< tambah << endl;
+   // cout << "hasil pengkurangan :"<< kurang << endl;
+   // cout << "hasil perkalian :"<< kali << endl;
+    //cout << "hasil pembagian :"<< bagi << endl;
+    //cout << "hasil sisa bagi :"<< modulo << endl;
+
+    /*relational operator,yaitu membandingkan dua nilai dan menghasilkan nilai boolean (true atau false)*/
+
+     //cout << (a==b)<<endl;
+    //cout << (a>b)<<endl;
+     //cout << (a>=b)<<endl;
+     //cout << (a<b)<<endl;
+     //cout << (a<=b)<<endl;
+     //cout << (a!=b)<<endl;
+
+     //^logical operator (&&,||,!)
+      //cout << (true && true)<< endl;
+      //cout << (true && false)<< endl;
+      //cout << (false && true)<< endl;
+      //cout << (false && false)<< endl;
+
+      //cout << (true || true)<< endl;
+      //cout << (true || false)<< endl;
+      //cout << (false || true)<< endl;
+      //cout << (false || false)<< endl;
+
+      //cout << |true << endl;
+      //cout << |false << endl;
+
+      /*bitwise operator (&,|,~,<<,>>)-- oprator biner,yaitu melakukan operasi pada bit data secara individual*/
+
+      //cout << (5&7)<<endl;
+      //cout << (5|7)<<endl;
+      //cout << (5^7)<<endl;
+      //cout << (~7)<<endl;
+      //cout << (7<<2)<<endl;
+      //cout << (7>>2)<<endl;
+
+    /*shorthand,yaitu operator yang lebih singkat untuk melakukan tugas yang sama dengan pernyataan yang lebih panjang*/
+    
+    //a += 7; // a = a + 7
+   // cout << a <<endl;
+
+   // a -= 7; // a = a - 7
+   // cout << a <<endl;
+
+    //a *= 7; // a = a * 7
+    //cout << a <<endl;
+
+    //a /= 7; // a = a / 7
+   // cout << a <<endl;
+
+   /*increament & decrement,yaitu pemrograman yang digunakan untuk menambah atau mengurangi nilai sebuah variabel*/
+   // pre increment
+   //cout << a << endl;
+   //cout << ++a << endl;
+   
+   //cout << b << endl;
+   //cout << ++b << endl;
+
+   /*post increament,yaitu pemrograman yang menambah nilai variabel sebesar 1 setelah variabel tersebut digunakan dalam ekspresi*/
+   //cout << a << endl;
+   //cout << a++ << endl;
+   //cout << a << endl;
+
+   //cout << b << endl;
+   //cout << b++ << endl;
+   //cout << b << endl;
+
+   /*pre increament,yaitu meningkatkan nilai suatu variabel sebesar 1 sebelum variabel tersebut digunakan dalam sebuah ekspresi*/
+   //cout << a << endl;
+   //cout << --a << endl;
+
+   //cout << b << endl;
+   //cout << --b << endl;
+
+   //cout << a << endl;
+   //cout << a-- << endl;
+   //cout << a << endl;
+   
+   //cout << b << endl;
+   //cout << b-- << endl;
+   //cout << b << endl;
+   
+}

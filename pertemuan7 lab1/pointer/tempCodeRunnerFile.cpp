9dd8fd29b7e4e71c136ee7fe697cda3c4c0bc1ba
Andina_YL <andinayulitama@gmail.com>
2025-05-25 18:31:22 +0700

    Pertemuan 7 : Pointer

---
+++ pertemuan7 lab1/pointer/tempCodeRunnerFile.cpp
@@ -0,0 +1,5 @@
+ cout<< " Alamat memory variabel number = "<< &number<<endl;
+    //  cout << "Isi variabel pointer number = "<<pointer_number<<endl;
+    //  cout << "Isi Variabel yang ditunjuk olej  pointer_number = "<< *pointer_number<<endl;
+    //  cout << "Alamat Memory variabel pointer_number = " << &pointer_number<<endl;
+    //  cout << endl;

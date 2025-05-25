9c4e688398f1af5f33dc290a481b6f4a31fe4607
Andina_YL <andinayulitama@gmail.com>
2025-05-25 17:40:07 +0700

    pertemuan6 lab1 directory

--- pertemuan6 lab1/headerCustom.h
+++
@@ -1,9 +0,0 @@
-#define u using namespace std; //menganti teks
-#define l <<
-#define o cout
-#define e endl;
-#define d double
-#define i int
-#define ayomain main()
-#define theand } 
-#define mulai {
